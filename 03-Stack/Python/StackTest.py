b882b5970ec2bc70b52f1bc5c13946e4fdcac14f
lucasrossiml <lucas.rossi@mercadolibre.com>
2017-09-12 21:15:47 -0300

    refactor

--- 03-Stack/Python/StackTest.py
+++ 03-Stack/Python/StackTest.py
@@ -10,69 +10,69 @@
 
 import unittest
 
-class Stack:
 
+class Stack:
     STACK_EMPTY_DESCRIPTION = 'Stack is empty'
 
     def __init__(self):
         self.contents = []
-    
+
     def push(self, anObject):
         self.contents.append(anObject)
-    
+
     def pop(self):
         if self.isEmpty():
             raise Exception(self.STACK_EMPTY_DESCRIPTION)
         return self.contents.pop(-1)
-    
+
     def top(self):
         if self.isEmpty():
             raise Exception(self.STACK_EMPTY_DESCRIPTION)
         return self.contents[-1]
-    
+
     def isEmpty(self):
         return self.size() == 0
-    
+
     def size(self):
         return len(self.contents)
-    
+
+
 class StackTest(unittest.TestCase):
-    
     def testStackShouldBeEmptyWhenCreated(self):
         stack = Stack()
-        
+
         self.assertTrue(stack.isEmpty())
 
     def testPushAddElementsToTheStack(self):
         stack = Stack()
         stack.push('something')
-        
+
         self.assertFalse(stack.isEmpty())
 
     def testPopRemovesElementsFromTheStack(self):
         stack = Stack()
         stack.push("Something")
         stack.pop()
-        
+
         self.assertTrue(stack.isEmpty())
-    
+
     def testPopReturnsLastPushedObject(self):
         stack = Stack()
         pushedObject = "Something"
         stack.push(pushedObject)
         self.assertEquals(pushedObject, stack.pop())
-    
+
     def testStackBehavesLIFO(self):
         firstPushed = "First"
         secondPushed = "Second"
         stack = Stack()
         stack.push(firstPushed)
         stack.push(secondPushed)
-        
-        self.assertEquals(secondPushed,stack.pop())
-        self.assertEquals(firstPushed,stack.pop())
+
+        self.assertEquals(secondPushed, stack.pop())
+        self.assertEquals(firstPushed, stack.pop())
         self.assertTrue(stack.isEmpty())
-    
+
     def testTopReturnsLastPushedObject(self):
         stack = Stack()
         pushedObject = "Something"
@@ -87,19 +87,19 @@ class StackTest(unittest.TestCase):
 
         stack.push(pushedObject)
 
-        self.assertEquals( 1,stack.size()) 
+        self.assertEquals(1, stack.size())
         stack.top()
-        self.assertEquals( 1,stack.size())
+        self.assertEquals(1, stack.size())
 
     def testCanNotPopWhenThereAreNoObjectsInTheStack(self):
         stack = Stack()
-        
+
         try:
             stack.pop()
             self.fail()
         except Exception as stackIsEmpty:
-            self.assertEquals(Stack.STACK_EMPTY_DESCRIPTION,stackIsEmpty.message)
-        
+            self.assertEquals(Stack.STACK_EMPTY_DESCRIPTION, stackIsEmpty.message)
+
     def testCanNotTopWhenThereAreNoObjectsInTheStack(self):
         stack = Stack()
 
@@ -107,7 +107,8 @@ class StackTest(unittest.TestCase):
             stack.top()
             self.fail()
         except Exception as stackIsEmpty:
-            self.assertEquals(Stack.STACK_EMPTY_DESCRIPTION,stackIsEmpty.message)
-    
+            self.assertEquals(Stack.STACK_EMPTY_DESCRIPTION, stackIsEmpty.message)
+
+
 if __name__ == "__main__":
     unittest.main()
